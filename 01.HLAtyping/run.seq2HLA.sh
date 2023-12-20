f0a169888ffaf2f55e4e3e5d855e845259b4ee49
lilab <147492704+lilab-qw@users.noreply.github.com>
2023-12-20 18:02:07 +0800

    Delete 01.HLAtyping/run.seq2HLA.sh

--- 01.HLAtyping/run.seq2HLA.sh
+++
@@ -1,4 +0,0 @@
-sample=$1
-fq1=/mnt/delta_DT/TSA/cell_line/RNA-seq/RNA-seq/${sample}/${sample}_1.filter.fastq.gz
-fq2=/mnt/delta_DT/TSA/cell_line/RNA-seq/RNA-seq/${sample}/${sample}_2.filter.fastq.gz
-python /mnt/delta_DT/TSA/cell_line/RNA-seq/RNA-seq/script/software/seq2HLA/seq2HLA.py  -1 $fq1 -2 $fq2   -r ${sample} -p 3
