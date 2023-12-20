bef84d0d8bcc2692a6971b4fe2c1984be3b4cea1
lilab <147492704+lilab-qw@users.noreply.github.com>
2023-12-20 17:57:22 +0800

    Delete run.quant.sh

--- run.quant.sh
+++
@@ -1,5 +0,0 @@
-sample=$1
-fq1=$2
-fq2=$3
-outdir=$4
-/mnt/delta_DT/TSA/cell_line/RNA-seq/RNA-seq/script/software/kallisto/kallisto  quant -i /mnt/delta_DT/TSA/cell_line/RNA-seq/RNA-seq/script/00.Alignment/homo_sapiens/transcriptome.idx -o $outdir/ -t 4 $fq1 $fq2
